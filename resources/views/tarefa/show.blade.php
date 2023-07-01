fd0ef0828b27f24d3f4889ca65ad222bec9e3937
Douglas Dg <douglas.pereira@aluno.unifametro.edu.br>
2023-07-01 11:07:02 -0300

    ...

--- resources/views/tarefa/show.blade.php
+++ resources/views/tarefa/show.blade.php
@@ -5,12 +5,19 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Visualizar Tarefa') }}</div>
+                <div class="card-header"><i class="fa-solid fa-eye"></i> {{ __('Visualizar Tarefa') }}</div>
 
                 <div class="card-body">
-                    <b> Atividade: </b>{{$tarefa->tarefa}} <br>
-                    <b> Data: </b>{{ date('d/m/Y', strtotime($tarefa->data_conclusao)) }} <br>
-                    <b> Descrição: </b>{{$tarefa->descricao}}
+                    <div class="callout-info">
+                        <b> Atividade: </b>{{$tarefa->tarefa}} <br>
+                        <b> Data: </b>{{ date('d/m/Y', strtotime($tarefa->data_conclusao)) }} <br>
+                        <b> Descrição: </b>{{$tarefa->descricao}}
+                    </div>
+                    
+                    <hr>
+                    <div class="mb-3 d-grid gap-2 d-md-flex justify-content-md-end">
+                        <a href="{{ route('tarefa.create') }}" class="btn btn-secondary">Voltar</a>
+                    </div>
                 </div>
             </div>
         </div>
